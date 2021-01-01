9feb16d9e5d48d0a580d9cd6a46dea24acd204f5
kaede <kaede.ito2001@gmail.com>
2021-01-01 08:57:27 -0500

    user login page

--- logCompareAPI/settings.py
+++ logCompareAPI/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'logCompareAPI.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -144,3 +144,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/api/'
