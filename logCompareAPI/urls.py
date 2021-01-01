12f08ca79649a9ca197cac61127444ae0154f2cd
kaede <kaede.ito2001@gmail.com>
2021-01-01 09:07:57 -0500

    moved template folder

--- logCompareAPI/urls.py
+++ logCompareAPI/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('^admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('maindb.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('accounts/', include('django.contrib.auth.urls'))
