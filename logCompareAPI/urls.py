a73d183f7657c65da1d938ca8faf8a87fbf39544
kaede <kaede.ito2001@gmail.com>
2021-01-01 19:27:46 -0500

    added signup page

--- logCompareAPI/urls.py
+++ logCompareAPI/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.authtoken.views import obtain_auth_token
+from maindb import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('maindb.urls')),
     path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('signup/', views.signup, name='signup')
 ]
