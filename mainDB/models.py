147461883fa71a846a6f86788244bfad6c575f09
Mapleia <mapleia01@gmail.com>
2021-01-08 07:28:42 -0500

    new version

--- mainDB/models.py
+++ mainDB/models.py
@@ -6,7 +6,7 @@ from django.db import models
 # Create your models here.
 class Encounter(models.Model) :
     Name = models.CharField(max_length=40)
-    TryID = models.IntegerField()
+    TryID = models.BigIntegerField()
     Gw2Build = models.IntegerField()
 
 class Fight(models.Model) :
@@ -15,15 +15,15 @@ class Fight(models.Model) :
     DPS = models.IntegerField()
     Archetype = models.CharField(max_length=7)
 
-    Protection = models.DecimalField(max_digits=6, decimal_places=3)
-    Regeneration = models.DecimalField(max_digits=6, decimal_places=3)
-    Swiftness = models.DecimalField(max_digits=6, decimal_places=3)
-    Fury = models.DecimalField(max_digits=6, decimal_places=3)
-    Vigor = models.DecimalField(max_digits=6, decimal_places=3)
-    Might = models.DecimalField(max_digits=6, decimal_places=3)
-    Aegis = models.DecimalField(max_digits=6, decimal_places=3)
-    Retaliation = models.DecimalField(max_digits=6, decimal_places=3)
-    Stability = models.DecimalField(max_digits=6, decimal_places=3)
-    Quickness = models.DecimalField(max_digits=6, decimal_places=3)
-    Resistance = models.DecimalField(max_digits=6, decimal_places=3)
-    Alacrity = models.DecimalField(max_digits=6, decimal_places=3)
+    Protection = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Regeneration = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Swiftness = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Fury = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Vigor = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Might = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Aegis = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Retaliation = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Stability = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Quickness = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Resistance = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
+    Alacrity = models.DecimalField(max_digits=6, decimal_places=3, default=0, editable=False)
