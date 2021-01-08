bf82a505a3bf6b5dc96eb0ba07c6a4c0538cce31
Mapleia <mapleia01@gmail.com>
2021-01-08 08:54:27 -0500

    reset again again

--- mainDB/serializers.py
+++ mainDB/serializers.py
@@ -1,16 +1,16 @@
 #serializers.py
 
 from rest_framework import serializers
-from .models import Encounters, Fights
+from .models import Encounter, Fight
 
 class EncounterSerializer(serializers.HyperlinkedModelSerializer) :
     class Meta: 
-        model = Encounters
+        model = Encounter
         fields = ('tryID', 'name', 'gw2Build')
 
 class FightSerializer(serializers.HyperlinkedModelSerializer) :
     class Meta:
-        model = Fights
+        model = Fight
         fields = ('tryID', 'account', 'DPS', 'archetype', 'protection','regeneration',
         'swiftness','fury','vigor','might','aegis','retaliation','stability',
         'quickness','resistance', 'alacrity')
