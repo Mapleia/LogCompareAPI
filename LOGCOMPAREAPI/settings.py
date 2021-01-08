3290c3c04c2108ac7d6cf2f6f208b06329023ca3
Mapleia <mapleia01@gmail.com>
2021-01-08 07:54:27 -0500

    set db default

--- LOGCOMPAREAPI/settings.py
+++ LOGCOMPAREAPI/settings.py
@@ -93,7 +93,10 @@ DATABASES = {
         'USER': 'root',
         'PASSWORD':os.getenv('DB_PASS'),
         'HOST':'localhost',
-        'PORT':'3306'
+        'PORT':'3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        }
     }
 }
 
