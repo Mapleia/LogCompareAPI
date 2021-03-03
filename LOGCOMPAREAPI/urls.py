443bc28ca475517d13f2a3b16349e07ef0875152
Mapleia <mapleia01@gmail.com>
2021-03-02 21:00:27 -0500

    fixed favicon url

--- LOGCOMPAREAPI/urls.py
+++ LOGCOMPAREAPI/urls.py
@@ -18,6 +18,8 @@ from django.urls import include, path
 from rest_framework import routers
 from mainDB import views 
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
@@ -33,4 +35,8 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('accounts/', include('accounts.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
